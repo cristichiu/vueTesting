19baea5ff33e84352de26f06cb876f64c0e2dc56
cristichiu <grajdiancristian@gmail.com>
2025-04-30 07:57:45 +0300

    ss

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,19 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Hello from '../views/hello.vue'
+import Hello from '@/views/hello.vue'
+import Salut from '@/views/salut.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/hello',
-      name: 'hello',
-      component: Hello,
-    },
-    {
-      path: '/salut',
-      name: 'salut',
-      component: () => import('../views/salut.vue'),
-    },
+    { path: '/hello', name: 'hello', component: Hello },
+    { path: '/salut', name: 'salut', component: Salut },
   ],
 })
 
